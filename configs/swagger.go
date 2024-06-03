b47db1e9e881cee38c34faf132c92951c45e21bc
changge <1282350001@qq.com>
2024-06-03 11:30:00 +0800

    feat: last change

--- configs/swagger.go
+++ configs/swagger.go
@@ -1,11 +1,15 @@
 /*
  * @Author: changge <changge1519@gmail.com>
  * @Date: 2022-10-25 14:40:56
- * @LastEditTime: 2022-10-25 15:23:27
+ * @LastEditTime: 2023-08-01 16:37:28
  * @Description: Do not edit
  */
 package configs
 
 type Swagger struct {
-	Host string `json:"host" yaml:"Host"`
+	Version  string   `json:"version" yaml:"Version"`
+	Host     string   `json:"host" yaml:"Host"`
+	BasePath string   `json:"base_path" yaml:"BasePath"`
+	Schemes  []string `json:"schemes" yaml:"Schemes"`
+	Title    string   `json:"title" yaml:"Title"`
 }
