983f11240e8954b5e6692c2a9d1c8871f7ab018a
changge <1282350001@qq.com>
2025-01-22 10:54:57 +0800

    refactor: go mod tidy

--- configs/config.go
+++ configs/config.go
@@ -1,19 +1,19 @@
 /*
  * @Author: changge <changge1519@gmail.com>
  * @Date: 2022-11-15 15:32:42
- * @LastEditTime: 2022-11-15 15:42:05
+ * @LastEditTime: 2025-01-22 10:38:54
  * @Description: Do not edit
  */
 package configs
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
 	"strings"
 
-	jsoniter "github.com/json-iterator/go"
 	"gopkg.in/yaml.v3"
 )
 
@@ -41,22 +41,22 @@ func GetConf() *Options {
 func InitConfig(fn string) *Options {
 	fd, err := os.Open(fn)
 	if err != nil {
-		log.Panicf(fmt.Sprintf("open conf file:%s error:%v", fn, err.Error()))
+		log.Panicf("%s", fmt.Sprintf("open conf file:%s error:%v", fn, err.Error()))
 	}
 	defer fd.Close()
 
-	content, err := ioutil.ReadAll(fd)
+	content, err := io.ReadAll(fd)
 	if err != nil {
-		log.Panicf(fmt.Sprintf("read conf file:%s error:%v", fn, err.Error()))
+		log.Panicf("%s", fmt.Sprintf("read conf file:%s error:%v", fn, err.Error()))
 	}
 
 	if strings.HasSuffix(fn, ".json") {
-		if err = jsoniter.Unmarshal(content, &options); err != nil {
-			log.Panicf(fmt.Sprintf("unmarshal conf file:%s error:%v", fn, err.Error()))
+		if err = json.Unmarshal(content, &options); err != nil {
+			log.Panicf("%s", fmt.Sprintf("unmarshal conf file:%s error:%v", fn, err.Error()))
 		}
 	} else if strings.HasSuffix(fn, ".yaml") {
 		if err = yaml.Unmarshal(content, &options); err != nil {
-			log.Panicf(fmt.Sprintf("unmarshal conf file:%s error:%v", fn, err.Error()))
+			log.Panicf("%s", fmt.Sprintf("unmarshal conf file:%s error:%v", fn, err.Error()))
 		}
 	}
 
